50667891f848525b2a82ab0664dea573054ec23f
Natalia Laszczkowska <natalia.laszczkowskax@gmail.com>
2024-01-28 18:15:58 +0100

    x

--- src/components/pages/About/About.jsx
+++ src/components/pages/About/About.jsx
@@ -1,7 +1,6 @@
 import { Container, Row, Col, Card } from 'react-bootstrap';
 import styles from './About.module.scss';
 
-
 const About = () => {
     return (
         <div className={styles.aboutSection}>
