2c27ee05a72c09cc43d5f95aeb0a0aeb816fc3cb
Natalia Laszczkowska <natalia.laszczkowskax@gmail.com>
2024-01-30 14:01:25 +0100

    Add basic structure of AprtmentPlans

--- src/components/pages/ApartmentPlans/ApartmentPlans.jsx
+++ src/components/pages/ApartmentPlans/ApartmentPlans.jsx
@@ -5,11 +5,41 @@ const ApartmentPlans = () => {
         <div className={styles.container}>
             <h2 className={styles.sectionTitle}>RZUTY MIESZKAŃ</h2>
             <div className={styles.apartmentGrid}>
-                {/* Tutaj umieść rzuty mieszkań jako obrazki */}
-                {/* Przykład: */}
-                <img src="/path/to/apartment1.jpg" alt="Rzut mieszkania 1" className={styles.apartmentImage} />
-                <img src="/path/to/apartment2.jpg" alt="Rzut mieszkania 2" className={styles.apartmentImage} />
-                {/* Dodaj więcej obrazków mieszkań */}
+                <div className={styles.imageContainer}>
+                    <div className={styles.imageInfo}>
+                        <h3 className={styles.sectionHeading}>Piwnica</h3>
+                        <p className={styles.description}>Informacje o piwnicy</p>
+                    </div>
+                    <img src="/images/piwnica.png" alt="Rzut piwnicy" className={styles.apartmentImage} />
+                </div>
+                <div className={styles.imageContainer}>
+                    <div className={styles.imageInfo}>
+                        <h3 className={styles.sectionHeading}>Parter</h3>
+                        <p className={styles.description}>Informacje o parterze</p>
+                    </div>
+                    <img src="/images/parter.png" alt="Rzut parteru" className={styles.apartmentImage} />
+                </div>
+                <div className={styles.imageContainer}>
+                    <div className={styles.imageInfo}>
+                        <h3 className={styles.sectionHeading}>1 piętro</h3>
+                        <p className={styles.description}>Informacje o 1 piętrze</p>
+                    </div>
+                    <img src="/images/1pietro.png" alt="Rzut 1 piętra" className={styles.apartmentImage} />
+                </div>
+                <div className={styles.imageContainer}>
+                    <div className={styles.imageInfo}>
+                        <h3 className={styles.sectionHeading}>2piętro</h3>
+                        <p className={styles.description}>Informacje o 2 piętrze</p>
+                    </div>
+                    <img src="/images/2pietro.png" alt="Rzut 2 piętra" className={styles.apartmentImage} />
+                </div>
+                <div className={styles.imageContainer}>
+                    <div className={styles.imageInfo}>
+                        <h3 className={styles.sectionHeading}>Poddasze</h3>
+                        <p className={styles.description}>Informacje o poddaszu</p>
+                    </div>
+                    <img src="/images/poddasze.png" alt="Rzut poddasza" className={styles.apartmentImage} />
+                </div>
             </div>
         </div>
     );
