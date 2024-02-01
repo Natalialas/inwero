ea61f86de9c665baa602d1e60528c4c9ae81892a
Natalia Laszczkowska <natalia.laszczkowskax@gmail.com>
2024-02-01 13:08:13 +0100

    Change Contact section

--- src/components/pages/Contact/Contact.jsx
+++ src/components/pages/Contact/Contact.jsx
@@ -5,25 +5,25 @@ const Contact = () => {
     return (
         <div className={styles.contactSection}>
             <h2 className={styles.sectionTitle}>KONTAKT</h2>
-            <div className={styles.contactInfo}>
                 <div className={styles.contactItemInfo}>
                     <p><b>INWERO Robert Skulimowski</b></p>
                     <p>NIP: 741-143-61-45</p>
                 </div>
-                <div className={styles.contactItem}>
-                    <FaMapMarkerAlt className={styles.contactIcon} />
-                    <p>Adres: ul. Dąbrowskiego 46, 14-300 Morąg</p>
-                </div>
-                <div className={styles.contactItem}>
-                    <FaPhoneAlt className={styles.contactIcon} />
-                    <p>Telefon: +48 509 267 206</p>
-                </div>
-                <div className={styles.contactItem}>
-                    <FaEnvelope className={styles.contactIcon} />
-                    <p>Email: robertbiuro@interia.pl</p>
+                <div className={styles.contactInfo}>
+                    <div className={styles.contactItem}>
+                        <FaMapMarkerAlt className={styles.contactIcon} />
+                        <p>Adres: ul. Dąbrowskiego 46, 14-300 Morąg</p>
+                    </div>
+                    <div className={styles.contactItem}>
+                        <FaPhoneAlt className={styles.contactIcon} />
+                        <p>Telefon: +48 509 267 206</p>
+                    </div>
+                    <div className={styles.contactItem}>
+                        <FaEnvelope className={styles.contactIcon} />
+                        <p>Email: robertbiuro@interia.pl</p>
+                    </div>
                 </div>
             </div>
-        </div>
     );
 };
 
