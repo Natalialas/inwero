04c783fa8607e8e4318417be7c9ea5b2bf6c84ff
Natalia Laszczkowska <natalia.laszczkowskax@gmail.com>
2024-07-03 11:04:42 +0200

    Add responsive styles for footer (collapsible menu)

--- src/components/views/NavBar/NavBar.jsx
+++ src/components/views/NavBar/NavBar.jsx
@@ -1,7 +1,9 @@
-import styles from './NavBar.module.scss';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { Link } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import { Link as ScrollLink } from 'react-scroll';
+import styles from './NavBar.module.scss';
 
 const NavBar = () => {
     const [activeSection, setActiveSection] = useState('home');
@@ -10,13 +12,13 @@ const NavBar = () => {
         const handleScroll = () => {
             const sections = document.querySelectorAll('section');
             const windowHeight = window.innerHeight;
-            
+
             sections.forEach(section => {
                 const sectionTop = section.offsetTop;
                 const sectionBottom = sectionTop + section.clientHeight;
                 const bottomOffset = section.id === 'kontakt' ? 100 : 0;
                 const topOffset = section.id !== 'kontakt' ? 70 : 0;
-                
+
                 if (window.scrollY + windowHeight >= sectionBottom - bottomOffset) {
                     setActiveSection(section.id);
                 } else if (window.scrollY >= sectionTop - topOffset && window.scrollY < sectionBottom - windowHeight) {
@@ -24,8 +26,7 @@ const NavBar = () => {
                 }
             });
         };
-        
-    
+
         window.addEventListener('scroll', handleScroll);
         return () => {
             window.removeEventListener('scroll', handleScroll);
@@ -33,19 +34,68 @@ const NavBar = () => {
     }, []);
 
     return (
-        <section className={styles.navBar}>
-            <div className={styles.icon}>
-                <Link to="/"> INWERO</Link>
-             </div>
-            <div className={styles.list}>
-                <ul>
-                    <li><ScrollLink to="home" smooth={true} duration={100} spy={true} offset={-70} className={activeSection === 'home' ? styles.linkActive : ''}>HOME</ScrollLink></li>
-                    <li><ScrollLink to="o-osiedlu" smooth={true} duration={100} spy={true} offset={-70} className={activeSection === 'o-osiedlu' ? styles.linkActive : ''}>O OSIEDLU</ScrollLink></li>
-                    <li><ScrollLink to="rzuty" smooth={true} duration={100} spy={true} offset={-70} className={activeSection === 'rzuty' ? styles.linkActive : ''}>RZUTY MIESZKAŃ</ScrollLink></li>
-                    <li><ScrollLink to="kontakt" smooth={true} duration={100} spy={true} offset={-70} className={activeSection === 'kontakt' ? styles.linkActive : ''}>KONTAKT</ScrollLink></li>
-                </ul>
-            </div>
-        </section>
+        <Navbar expand="lg" fixed="top" className={styles.navBar}>
+            <Container>
+                <Navbar.Brand>
+                    <Link to="/" className={styles.iconLink}>
+                        INWERO
+                    </Link>
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+                    <Nav>
+                        <Nav.Link as="span">
+                            <ScrollLink
+                                to="home"
+                                smooth={true}
+                                duration={100}
+                                spy={true}
+                                offset={-70}
+                                className={`${styles.navLink} ${activeSection === 'home' ? styles.linkActive : ''}`}
+                            >
+                                HOME
+                            </ScrollLink>
+                        </Nav.Link>
+                        <Nav.Link as="span">
+                            <ScrollLink
+                                to="o-osiedlu"
+                                smooth={true}
+                                duration={100}
+                                spy={true}
+                                offset={-70}
+                                className={`${styles.navLink} ${activeSection === 'o-osiedlu' ? styles.linkActive : ''}`}
+                            >
+                                O OSIEDLU
+                            </ScrollLink>
+                        </Nav.Link>
+                        <Nav.Link as="span">
+                            <ScrollLink
+                                to="rzuty"
+                                smooth={true}
+                                duration={100}
+                                spy={true}
+                                offset={-70}
+                                className={`${styles.navLink} ${activeSection === 'rzuty' ? styles.linkActive : ''}`}
+                            >
+                                RZUTY MIESZKAŃ
+                            </ScrollLink>
+                        </Nav.Link>
+                        <Nav.Link as="span">
+                            <ScrollLink
+                                to="kontakt"
+                                smooth={true}
+                                duration={100}
+                                spy={true}
+                                offset={-70}
+                                className={`${styles.navLink} ${activeSection === 'kontakt' ? styles.linkActive : ''}`}
+                            >
+                                KONTAKT
+                            </ScrollLink>
+                        </Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 };
 
